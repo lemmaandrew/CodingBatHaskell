48422b7a3703c601a7d27b83c81d2d8169043aa9
Peter Andrew <peterjayandrew@gmail.com>
2020-06-21 13:46:04 -0500

    Made Main.hs write to files, and removed printSite

--- Main.hs
+++ Main.hs
@@ -1,17 +1,38 @@
 -- The Main module, built to download and compile the programs from https://codingbat.com/java
 
-import Download
-
 import Data.List (intercalate)
-
+import Debug.Trace
+import System.Directory
+import System.IO
 import Text.Printf
 
-{-
--- Ugly as sin but only needed for printing purposes
--- Will not be included in the final build
--}
-printSite :: IO ()
-printSite = mapM_ (\(catName,problems) -> printf "\n\n\n\n--------------\n%s\n\n%s" catName (intercalate "\n\n" $ map (\(probName,probStr) -> probName ++ "\n" ++ probStr) problems))
-            =<< (map unpackCategory) <$> getSite
+import Download
 
-main = printSite
+writeProblem :: FilePath -> Problem -> IO ()
+writeProblem directory pbm =
+    let (fileName,fileContents) = compileProblem pbm
+        path = printf "%s/%s.hs" directory fileName
+    in do
+        createDirectoryIfMissing False directory
+        putStrLn $ "Writing: " ++ path
+        handle <- openFile path WriteMode
+        hPutStrLn handle fileContents
+        hClose handle
+
+writeCategory :: FilePath -> Category -> IO ()
+writeCategory directory (Category name problems) = mapM_ go problems where
+    go (Just pbm) = writeProblem (directory ++ "/" ++ name) pbm
+    go (Nothing) = trace ("Failed to write problem in category " ++ name) (return ())
+
+main :: IO ()
+main = do
+    putStrLn "Are you sure you want to run Main? It will overwrite all files you have worked on [y/N]"
+    response <- getLine
+    if response == "y"
+        then do
+            putStrLn "Downloading https://codingbat.com/java ..."
+            createDirectoryIfMissing False "./CodingBat"
+            mapM_ (\cat@(Category name _) ->
+                        trace ("Writing " ++ name) (writeCategory "./CodingBat" cat)) =<< getSite
+            putStrLn "Finished!"
+    else putStrLn "Exiting..."
